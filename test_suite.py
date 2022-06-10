89076770de22e0f59d0ac46861fc9b85e1c7ae6b
Lynn <lynn@foldr.moe>
2022-06-11 00:45:44 +0200

    Polish it up a little

--- test_suite.py
+++ test_suite.py
@@ -12,11 +12,15 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print("Download https://drive.google.com/file/d/1kvFxYh2fo3bHfVj1OKKLX8YzuxswHvYg/view?usp=sharing and extract it to a directory named 'problems' in this folder.")
         sys.exit(1)
+
+    passes = fails = 0
     for prob in sorted(problems, key=lambda x: x.name):
         # Luck / weird problems
-        if prob.name in ["123", "123 Reloaded", "Error", "Helloworld", "Inverse Quine", "Not Quine", "Quine", "Timeout"]: continue
-        # Ruby version stuff
+        if prob.name in ["123", "123 Reloaded", "Error", "Inverse Quine", "Not Quine", "Palindromic Quine", "Quine", "Timeout"]: continue
+        # Ruby version / float / rational trickery
         if prob.name in ["area of triangle", "0_5 broken keyboard", "Cancel fractions", "Equal Temperament", "MIDI note number to frequency", "Numloop"]: continue
+        # Sisyphus's dump was unluckily case-insensitive, so these have wrong answers mixed in:
+        if prob.name.lower() in ["helloworld", "christmas tree", "multiplication table"]: continue
 
         if sys.argv[1:] and prob.name < sys.argv[1]: continue
         if not prob.is_dir(): continue
@@ -44,13 +48,13 @@ if __name__ == "__main__":
                 if b'#{' in soln_string: continue
                 pi = problem["input"].encode("utf-8").replace(b"\r\n", b"\n")
                 po = problem["output"].encode("utf-8").replace(b"\r\n", b"\n")
-                try:
-                    #print(soln.path)
-                    soln_output = subprocess.run(["ruby", "--encoding", "ASCII-8BIT", "golfscript.rb", soln.path], input=pi, stdout=subprocess.PIPE).stdout
-                    if soln_output.rstrip() == po.rstrip():
-                        print(soln.path, "\x1b[32mOK\x1b[0m")
-                    else:
-                        print(soln.path, "\x1b[31mFAIL\x1b[0m")
-                        print(soln_output[:100], po[:100])
-                except subprocess.CalledProcessError:
-                    print(soln.path, "CRASH")
+                soln_output = subprocess.run(["ruby", "--encoding", "ASCII-8BIT", "golfscript.rb", soln.path], input=pi, stdout=subprocess.PIPE).stdout
+                if soln_output.rstrip() == po.rstrip():
+                    print("\x1b[32mPASS\x1b[0m", soln.path)
+                    passes += 1
+                else:
+                    print("\x1b[31mFAIL\x1b[0m", soln.path)
+                    print(soln_output[:100], po[:100])
+                    fails += 1
+    print(f"{passes} passes, {fails} fails")
+
