dab181917d40818fa9c7013bf3917846168172bf
Lynn <lynn@foldr.moe>
2022-06-19 19:46:07 +0200

    Test suite tweaks

--- test_suite.py
+++ test_suite.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 
 min_date = 1199142000 # january 1, 2008
+compact = False
 
 if __name__ == "__main__":
     try:
@@ -14,11 +15,12 @@ if __name__ == "__main__":
         sys.exit(1)
 
     passes = fails = 0
+    last_pass = False
     for prob in sorted(problems, key=lambda x: x.name):
         # Luck / weird problems
         if prob.name in ["123", "123 Reloaded", "Error", "Inverse Quine", "Not Quine", "Palindromic Quine", "Quine", "Timeout"]: continue
         # Ruby version / float / rational trickery
-        if prob.name in ["area of triangle", "0_5 broken keyboard", "Cancel fractions", "Equal Temperament", "MIDI note number to frequency", "Numloop"]: continue
+        if prob.name in ["area of triangle", "Concentric Circles", "0_5 broken keyboard", "Cancel fractions", "Equal Temperament", "MIDI note number to frequency", "Numloop"]: continue
         # Sisyphus's dump was unluckily case-insensitive, so these have wrong answers mixed in:
         if prob.name.lower() in ["helloworld", "christmas tree", "multiplication table"]: continue
 
@@ -36,25 +38,36 @@ if __name__ == "__main__":
                 problem = json.loads(fp.read())
         except json.decoder.JSONDecodeError:
             continue
-        
         for soln in os.scandir(os.path.join(prob, "gs")):
             date = int(soln.name.split(".")[-2])
             if date < min_date: continue
+            # I'm sorry! https://github.com/shinh/ags/issues/4
             if 1531576304 - 10000 < date < 1531576304 + 10000 and soln.name.startswith("lynn"): continue
             if problem["output"] and 1 <= os.path.getsize(soln.path):
                 soln_string = open(soln.path, 'rb').read()
+                try: soln_string.decode("utf-8")
+                except: continue
+                if b'\0' in soln_string: continue
                 if b'rand' in soln_string: continue
                 if b'../s/' in soln_string: continue
                 if b'#{' in soln_string: continue
                 pi = problem["input"].encode("utf-8").replace(b"\r\n", b"\n")
                 po = problem["output"].encode("utf-8").replace(b"\r\n", b"\n")
-                soln_output = subprocess.run(["ruby", "--encoding", "ASCII-8BIT", "golfscript.rb", soln.path], input=pi, stdout=subprocess.PIPE).stdout
-                if soln_output.rstrip() == po.rstrip():
-                    print("\x1b[32mPASS\x1b[0m", soln.path)
-                    passes += 1
-                else:
-                    print("\x1b[31mFAIL\x1b[0m", soln.path)
-                    print(soln_output[:100], po[:100])
+                try:
+                    soln_output = subprocess.run(["ruby", "--encoding", "ASCII-8BIT", "golfscript.rb", soln.path], input=pi, stdout=subprocess.PIPE, timeout=5).stdout
+                    if soln_output.replace(b"\r\n", b"\n").rstrip() == po.rstrip():
+                        print(("\x1b[1A\x1b[K"*compact*last_pass) + "\x1b[32mPASS\x1b[0m", soln.path)
+                        last_pass = True
+                        passes += 1
+                    else:
+                        print("\x1b[31mFAIL\x1b[0m", soln.path)
+                        print(soln_output[:100], po[:100])
+                        last_pass = False
+                        fails += 1
+                except subprocess.TimeoutExpired:
+                    print("\x1b[33mSLOW\x1b[0m", soln.path)
+                    last_pass = False
                     fails += 1
+
     print(f"{passes} passes, {fails} fails")
 
